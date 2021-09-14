c39f2b74dac5f73f4d62b2ab10655869e2b116d6
lucaspedronet <lucaspedrolbg@gmail.com>
2021-09-14 15:10:37 -0300

    Config: (global theme, new pages Home or Status)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  IBMPlexSans_400Regular,
+  IBMPlexSans_600SemiBold,
+  IBMPlexSans_500Medium,
+  IBMPlexSans_700Bold,
+} from '@expo-google-fonts/ibm-plex-sans';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [fontLoading] = useFonts({
+    IBMPlexSans_600SemiBold,
+    IBMPlexSans_400Regular,
+    IBMPlexSans_500Medium,
+    IBMPlexSans_700Bold,
+  });
+
+  if (!fontLoading) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
---
+++ src/global/theme.ts
@@ -0,0 +1,68 @@
+const brandColors = {
+  primary: '#010062',
+  secondary: '#1D1B7E',
+  successDefault: '#56B936',
+  successLighter: '#AEDFA6',
+  textPrimary: '#CAC9FB',
+  text: '#37403B',
+  white: '#ffffff',
+  back: 'rgba(0, 0, 0, 1)',
+  'dark-dark': '#37403B',
+};
+
+const gridScale = {
+  total: 100,
+  '1/4': 33.33,
+  xs: 16,
+  md: 20,
+  lg: 24,
+  xl: 28,
+  xxl: 32,
+  xxxl: 36,
+  display: 40,
+};
+
+const fonts = {
+  IBM_Regular: 'IBMPlexSans_400Regular',
+  IBM_Medium: 'IBMPlexSans_500Medium',
+  IBM_SemiBold: 'IBMPlexSans_600SemiBold',
+  IBM_Bold: 'IBMPlexSans_700Bold',
+};
+
+const fontScale = {
+  tagline: 10,
+  caption: 11,
+  xxxs: 12,
+  xxs: 14,
+  xs: 16,
+  sm: 18,
+  md: 20,
+  lg: 24,
+  xl: 28,
+  xxl: 32,
+  xxxl: 36,
+  display: 40,
+};
+
+export const theme = {
+  screen: {
+    background: brandColors.white,
+    paddingSmall: gridScale.xs,
+    paddingDefault: gridScale.md,
+    paddingLarge: gridScale.lg,
+  },
+  card: {
+    background: brandColors.white,
+    backgroundGree: brandColors.successDefault,
+    paddingSmall: gridScale.xs,
+    paddingDefault: gridScale.md,
+    paddingLarge: gridScale.lg,
+  },
+  fontColor: {
+    default: brandColors.text,
+    title: brandColors.textPrimary,
+    titleSecondary: brandColors.white,
+  },
+  fonts,
+  fontScale,
+};
